8d3edce802bfd88f87c2ab2d9d8725ef4528e4cb
Evgen <redrif48@gmail.com>
2024-06-18 11:12:33 +0200

    add new endpoint to get all tasks and create new task

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from apps.tasks.views.tag_views import TagListAPIView, TagDetailApiView
+from apps.tasks.views.task_view import AllTasksListAPIView
 
 urlpatterns = [
+    path('', AllTasksListAPIView.as_view()),
     path('tags/', TagListAPIView.as_view()),
-    path('tags/<int:tag_id>', TagDetailApiView.as_view())
+    path('tags/<int:tag_id>', TagDetailApiView.as_view()),
 ]
+
