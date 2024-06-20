5535c4fed9acaa656d66df3d73edf02be6dbcf15
VictoriaSpecht <spechtvictoria1@gmail.com>
2024-06-20 09:43:03 +0200

    add update project_file_views

--- apps/projects/views/project_files_views.py
+++ apps/projects/views/project_files_views.py
@@ -3,14 +3,39 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import ListCreateAPIView, get_object_or_404
 
-from apps.projects.models import Project
-from apps.projects.serializers.project_file_serializers import CreateProjectFileSerializer
+from apps.projects.models import Project, ProjectFile
+from apps.projects.serializers.project_file_serializers import (
+    CreateProjectFileSerializer,
+    AllProjectFileSerializer
+)
 
 
 class ProjectFileListGenericView(ListCreateAPIView):
+    def get_queryset(self):
+        project_name = self.request.query_params.get("project_name")
+        if project_name:
+            return ProjectFile.objects.filter(
+                project__name=project_name
+            )
+        return ProjectFile.objects.all()
+
     def get_serializer_class(self, *args, **kwargs):
-        if self.request.method == 'POST':
-            return CreateProjectFileSerializer
+        if self.request.method == 'GET':
+            return AllProjectFileSerializer
+        return CreateProjectFileSerializer
+
+    def get(self, request: Request, *args, **kwargs)-> Response:
+        project_files = self.get_queryset()
+        if not project_files.exists():
+            return Response(
+                data=[],
+                status=status.HTTP_204_NO_CONTENT
+            )
+        serializer = self.get_serializer(project_files, many=True)
+        return Response(
+            data=serializer.data,
+            status=status.HTTP_200_OK
+        )
 
     def post(self, request: Request, *args, **kwargs) -> Response:
         file = request.FILES.get("file", None)
