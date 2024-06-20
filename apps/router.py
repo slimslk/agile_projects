94209ea56729ff7a3a573a158c3201c963d9ee3f
Dimm <slim.slk@gmail.com>
2024-06-20 11:23:01 +0200

    add user view and endpoint

--- apps/router.py
+++ apps/router.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('tasks/', include('apps.tasks.urls')),
-    path('projects/', include('apps.projects.urls'))
+    path('projects/', include('apps.projects.urls')),
+    path('users/', include('apps.users.urls'))
 ]
